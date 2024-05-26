59b3ba8109cb1281928058444d61c762bbd69231
waliulislamtonmoy <tonmoykhandiu@gmail.com>
2024-05-27 00:12:39 +0600

    upload project in githu

---
+++ script.js
@@ -0,0 +1,73 @@
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let price = document.querySelector(".price");
+let roomNumber1 = document.getElementById("roomNumber1");
+let roomNumber = document.querySelector(".roomNumber11");
+let signupbtn1 = document.querySelector(".signup-btn-1");
+let signupbtn = document.querySelector(".signup-btn");
+
+let proMessage = document.getElementById("proMessage");
+let message = document.getElementById("message");
+// console.log(proMessage);
+
+// proMessage.innerHTML = "<h1>thanks proMessage</h1>";
+
+let n = 1;
+let roomprice = 199;
+
+plus.addEventListener("click", () => {
+  n++;
+  AvarageRoomPrice = roomprice * n;
+  price.innerText = AvarageRoomPrice;
+  roomNumber1.innerText = n;
+});
+
+minus.addEventListener("click", () => {
+  if (n > 0) {
+    n--;
+  }
+
+  AvarageRoomPrice = roomprice * n;
+  price.innerText = AvarageRoomPrice;
+  roomNumber1.innerText = n;
+});
+
+//pro-package start
+
+let ProPlus = document.querySelector(".ProPlus");
+let ProMinus = document.querySelector(".ProMinus");
+let ProPrice = document.querySelector(".ProPrice");
+let ProRoomNumber1 = document.getElementById("ProRoomNumber1");
+let ProRoomNumber = document.querySelector(".ProRoomNumber11");
+
+console.log(ProRoomNumber);
+
+let ProRoom = 1;
+let ProRoomprice = 249;
+
+ProPlus.addEventListener("click", () => {
+  ProRoom++;
+  AvarageRoomPrice = ProRoomprice * ProRoom;
+  ProPrice.innerText = AvarageRoomPrice;
+  ProRoomNumber1.innerText = ProRoom;
+});
+
+ProMinus.addEventListener("click", () => {
+  if (ProRoom > 0) {
+    ProRoom--;
+  }
+
+  AvarageRoomPrice = ProRoomprice * ProRoom;
+  ProPrice.innerText = AvarageRoomPrice;
+  ProRoomNumber1.innerText = ProRoom;
+});
+
+signupbtn1.addEventListener("click", () => {
+  message.innerHTML = `<p>Thank you for chosing ${n}  Room</p>`;
+  roomNumber.innerText = n;
+});
+
+signupbtn.addEventListener("click", () => {
+  ProRoomNumber.innerText = ProRoom;
+  proMessage.innerHTML = `<p>Thank you for chosing ${ProRoom} Pro Room</p>`;
+});
